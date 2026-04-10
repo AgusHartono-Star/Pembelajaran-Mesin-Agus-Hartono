189725153cce70c17a215bc5139c0d5373f4e1d4
Agus Hartono <agushartono1705@gmail.com>
2026-04-10 10:54:26 +0800

    update

--- app_predict.py
+++ app_predict.py
@@ -136,6 +136,8 @@ def predict():
         print(f"❌ Error Server: {e}")
         return jsonify({"error": str(e)}), 500
 
+
 if __name__ == "__main__":
-    # Jalankan server agar bisa diakses Ngrok
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    # Menangkap port dari server Render, jika tidak ada pakai 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
